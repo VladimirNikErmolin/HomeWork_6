25b1b9c6d1ff3b887ba14f738c53a98b30b6551c
Vladimir Ermolin <w.ermolin@yahoo.com>
2022-09-23 13:35:35 +0300

    greater than zero

---
+++ greaterThanZero_41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] elements = CreatArray();
+PrintArray(elements);
+int countMoreZero = FindMoreThanZero(elements);
+System.Console.WriteLine($"Количество чисел больше нуля равно {countMoreZero}");
+
+int[] CreatArray()
+{
+    Console.WriteLine("Введите количество элементов M массива:");
+    int elementsCount = int.Parse(Console.ReadLine()!);
+    int[] array = new int[elementsCount];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Массив: ");
+    foreach (int number in array)
+    {
+        Console.Write($"{number} ");
+    }
+    System.Console.WriteLine();
+}
+
+int FindMoreThanZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count += 1;
+    }
+    return count;
+}
